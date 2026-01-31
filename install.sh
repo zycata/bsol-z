f5105dceca62dcddd17370753339c35e24a1fcea
zycata <hyperconger@gmail.com>
2026-01-31 11:05:23 -0800

    install.sh complete

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo "Welcome to installing the blue screen of life (yippee)"
+GRUB_CFG="/etc/default/grub"
+THEME_NAME="bsol"
+echo "Welcome to installing the blue screen of life"
 
 # user must script as root
 if [[ $(id -u) -ne 0 ]]; then
@@ -9,29 +10,56 @@ if [[ $(id -u) -ne 0 ]]; then
 else
 	echo "Script is running with root permissions... continuing"
 fi
-echo "riyal or fakeh"
 
 # check if the grub folder is called grub/ or grub2/
+echo "looking for grub folder"
 if [ -d /boot/grub ]; then
-    grub_path="/boot/grub"
+    GRUB_PATH="/boot/grub"
 elif [ -d /boot/grub2 ]; then
-    grub_path="/boot/grub2"
+    GRUB_PATH="/boot/grub2"
 else
     echo "Can't find a /boot/grub or /boot/grub2 folder. Exiting."
     exit
 fi
+echo "grub folder is: $GRUB_PATH"
+THEME_PATH="$GRUB_PATH/themes"
 
-grub_path="."
-echo "Grub Path is: $grub_path"
+# making the path and copying files
+mkdir -p "$THEME_PATH"
+cp -ru ./$bsol/ $THEME_PATH
+if [ $? -eq 0 ]; then
+    echo "sucessfully copied files to: $THEME_PATH"
+else
+    echo "Failed to copy files to: $THEME_PATH"
+    exit 1
+fi
+
+# write to grub config file
+echo "Writing to config file: $GRUB_CFG"
+if grep -q "GRUB_THEME=" "$GRUB_CFG"; then
+    echo "writing to grub"
+    sed -i "s|^GRUB_THEME=.*|GRUB_THEME=\"${THEME_PATH}/${THEME_NAME}/theme.txt\"|" "$GRUB_CFG"
+    
+    # this one here checks if the line is commented out uhh peak programmering for sure
+    sed -i "s|^#GRUB_THEME=.*|GRUB_THEME=\"${THEME_PATH}/${THEME_NAME}/theme.txt\"|" "$GRUB_CFG"
+else
+	echo "Failed to insert theme into $GRUB_CFG (do it manually yourself)"
+	exit
+fi
 
 
 
-theme_path="$grub_path/themes/bsol"
+echo "Running Os Prober"
+if command -v grub-mkconfig > /dev/null 2>&1; then
+    echo "GRUB is installed. Updating config..."
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+else
+    echo "GRUB not found. Skipping boot update."
+    exit 1
+fi
 
-mkdir -p "$grub_path/themes"
-
-cp -ru ./bsol/ $grub_path/themes/
 
 
 
-
+echo "---- Install script complete ----"
+echo "Credits to harishnkr for the original projects: https://github.com/harishnkr/bsol"
